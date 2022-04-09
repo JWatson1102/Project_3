90defc49c6b133e1868eff7c2f42b013d3f11bb2
amycastillon <amy.castillon13@gmail.com>
2022-04-09 10:44:52 -0500

    created sql schema

---
+++ bigfoot_data/cleaned_bigfoot_erd.sql
@@ -0,0 +1,11 @@
+CREATE TABLE bigfoot_sightings(
+	id int,
+	observed varchar,
+	county varchar,
+	state varchar,
+	latitude float,
+	longitude float,
+	date date 
+);
+
+SELECT * FROM bigfoot_sightings;
