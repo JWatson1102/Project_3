559e2f47cf276dbaf2397023aab67b3d05b3d504
JWatson1102 <irishwatsonj>
2022-04-12 20:57:04 -0500

    added additional webpages to flask file

--- app/app.py
+++ app/app.py
@@ -6,5 +6,22 @@ app.static_folder = 'static'
 def home():
     return render_template('index.html')
 
+@app.route('/bigfoot_files.html')
+def bigfoot():
+    return render_template('bigfoot_files.html')
+
+@app.route('/ghost_files.html')
+def ghost():
+    return render_template('ghost_files.html')
+
+@app.route('/ufo_files.html')
+def ufo():
+    return render_template('ufo_files.html')
+
+@app.route('/index.html')
+def back_home():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run()
