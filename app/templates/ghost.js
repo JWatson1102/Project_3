38d926ab35926ef8d9acbaad2ba069063a0bab0b
Marquetta <73968023+quettamar@users.noreply.github.com>
2022-04-12 21:52:53 -0500

    updated map to clustermap

--- app/templates/ghost.js
+++ app/templates/ghost.js
@@ -12,23 +12,29 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //   // An array containing each city's name, location, and population
 // var ghostFiles = $.getJSON("../../haunted_data/haunted.json", function(){
 //     console.log("success");
+// fetch("../../haunted_data/haunted.json")
+//     .then(response => response.json())
+//     .then(function (json) {
+//         console.log(json)
+//         for (var i = 0; i < json.length; i++) {
+//             var city = json[i];
+//             L.marker([city.latitude, city.longitude])
+//                 .bindPopup(`${city.city}<hr><p>${city.description}</p>`)
+//                 .addTo(myMap);
+//         }
+//     });
+
 fetch("../../haunted_data/haunted.json")
     .then(response => response.json())
     .then(function (json) {
-        console.log(json)
+        var markers = L.markerClusterGroup();
         for (var i = 0; i < json.length; i++) {
-            var city = json[i];
-            L.marker([city.latitude, city.longitude])
-                .bindPopup(`${city.city}<hr><p>${city.description}</p>`)
-                .addTo(myMap);
+            var sighting = json[i];
+            // var location = ([city.latitude, city.longitude])
+            if (sighting) {
+                markers.addLayer(L.marker([sighting.latitude, sighting.longitude])
+                    .bindPopup(sighting.city + "<hr><p>" + sighting.description + "</p>"));
+            }
         }
+    myMap.addLayer(markers);
     });
-
-
-//   // Looping through the cities array, create one marker for each city, bind a popup containing its name and population, and add it to the map.
-//   for (var i = 0; i < cities.length; i++) {
-//     var city = cities[i];
-//     L.marker(city.location)
-//       .bindPopup(`<h1>${city.name}</h1> <hr> <h3>Population ${city.population.toLocaleString()}</h3>`)
-//       .addTo(myMap);
-//   }
