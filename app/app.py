af332368d2b5d74b89d0bcb6e7a1c40432abf5d7
JWatson1102 <irishwatsonj>
2022-04-17 19:51:07 -0500

    made changes to app.py to display maps using sqlite database

--- app/app.py
+++ app/app.py
@@ -45,13 +45,13 @@ def back_home():
 @app.route("/api/ufo_json")
 def ufo_json():
 
-    conn = psycopg2.connect(postgres_url)
+    conn = psycopg2.connect("..\sqlite test\lat_and_lon.sqlite")
     cursor = conn.cursor()
 
-    cursor.execute(f'''SELECT * from {table_name}''')
+    cursor.execute(f'''SELECT * from ufo''')
 
     results = cursor.fetchall()
-    ufo_db = [ {"Description": result[0], "Shape": result[1],"Duration": result[2], "City": result[3],"State": result[4], "Date": result[5],  "Time": result[6], "Latitude": result[7] , "Longitude":result[8]} for result in results]
+    ufo_db = [{'latitude':result[1], 'longitude':result[2]} for result in results]
 
     conn.close()
 
@@ -60,14 +60,14 @@ def ufo_json():
     
 @app.route("/api/haunted_places")
 def sqlite_web_api():
-    conn = sqlite3.connect(f'db/{db_name}.db')
+    conn = sqlite3.connect('..\sqlite test\lat_and_lon.sqlite')
 
     cursor = conn.cursor()
 
-    cursor.execute(f'''SELECT * from {table_name2}''')
+    cursor.execute(f'''SELECT * from hauntings''')
 
     results = cursor.fetchall()
-    ghost_files_db = [{"city":result[0], "country":result[1], "description":result[2], "location":result[3], "state":result[4], "state_abbrev":result[5], "longitude":result[6], "latitude":result[7]} for result in results]
+    ghost_files_db = [{'latitude':result[1], 'longitude':result[2]} for result in results]
 
     conn.close()
 
@@ -77,14 +77,13 @@ def sqlite_web_api():
 @app.route("/api/bigfoot_data")
 def bigfoot_json():
 
-    conn = psycopg2.connect(postgres_url)
+    conn = psycopg2.connect("..\sqlite test\lat_and_lon.sqlite")
     cursor = conn.cursor()
 
-    cursor.execute(f'''select * from {table_name3};''')
+    cursor.execute(f'''select * from bigfoot;''')
     print("this is a test")
     results = cursor.fetchall()
-    bigfoot_db = [{"observed":result[0], "county":result[1], "state":result[2], "latitude":result[3], "longitude":result[4]} for result in results]
-    
+    bigfoot_db = [{'latitude':result[1], 'longitude':result[2]} for result in results]
     conn.close()
 
     print("responding to /postgresql-web-api route request")
